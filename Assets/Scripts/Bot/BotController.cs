8286f7419b134e3b387dafc62681d36232d77382
Şamil <sml.ozclk@gmail.com>
2023-07-29 16:19:19 +0300

    remove unnecessary codes from bot controller

--- Assets/Scripts/Bot/BotController.cs
+++ Assets/Scripts/Bot/BotController.cs
@@ -8,21 +8,12 @@ public class BotController : PlayerControllerBase
     [SerializeField]
     private TextMeshProUGUI _playerName;
 
-    [SerializeField]
-    private Transform hand;
-
-    [SerializeField]
-    private TextMeshProUGUI _playerScoreText;
-
-    [SerializeField]
-    private TextMeshProUGUI _playerMoneyText;
-
     public override void SetPlayer(IPlayer player)
     {
         base.SetPlayer(player);
         _playerName.text = "Bot";
-        _playerScoreText.text = player.Score.ToString();
-        _playerMoneyText.text = player.Currency.Amount.ToString();
+        _scoreText.text = player.Score.ToString();
+        _currencyText.text = player.Currency.Amount.ToString();
     }
 
     public class Factory : PlaceholderFactory<BotController>
